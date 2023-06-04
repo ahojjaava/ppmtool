31d7c9c5be7e3a59680e8a07df786f893563b7ce
Hsin-Lan Yu <hsinlanyu@outlook.com>
2023-06-04 21:42:10 +0200

    br 1 - Project Object & Project Repository

---
+++ main/java/io/hly/ppmtool/domain/Project.java
@@ -0,0 +1,99 @@
+package io.hly.ppmtool.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date create_date;
+    private Date update_date;
+
+    public Project() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Date getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(Date create_date) {
+        this.create_date = create_date;
+    }
+
+    public Date getUpdate_date() {
+        return update_date;
+    }
+
+    public void setUpdate_date(Date update_date) {
+        this.update_date = update_date;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.create_date = new Date();
+    }
+
+
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.update_date = new Date();
+    }
+}
