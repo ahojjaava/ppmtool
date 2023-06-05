ec2514887d840e07567932072bcb4c985ae969c2
Hsin-Lan Yu <hsinlanyu@outlook.com>
2023-06-05 18:07:21 +0200

    br 2 - create Project services and Controller

---
+++ main/java/io/hly/ppmtool/services/ProjectService.java
@@ -0,0 +1,18 @@
+package io.hly.ppmtool.services;
+
+import io.hly.ppmtool.domain.Project;
+import io.hly.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        //Logic
+        return projectRepository.save(project);
+    }
+}
